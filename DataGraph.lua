6db06e48f54ea56731e1f879baf99e91bd9a5097
Armando Arce <armando.arce@gmail.com>
2018-10-30 20:59:05 -0600

    Error cleaning

--- DataGraph.lua
+++ DataGraph.lua
@@ -1,72 +1,79 @@
-DataGraph = {}
+local write = io.write
+
+local DataGraph = {}
 
 DataGraph = function()
-  local table = {vertex={},edge={},first={},last={},next={}}
+  local table = {vertex={},edge={},first={},last={},next={},adjacent={},mark={}}
   table.addVertex = function(label,fields)
-   local n = #table.vertex + 1
+    local n = #table.vertex + 1
     table.vertex[n] = label
     table.first[n] = 0
     table.last[n] = 0
     if fields then
       for k,v in pairs(fields) do
+        if table[k] == nil then
+          table[k] = {}
+        end
         table[k][n]=v
       end
     end
     return n
   end
   table.addEdge = function(vdx1,vdx2,label)
-    local n = #table.edge + 1
-    table.edge[n] = label
+    local m = #table.edge + 1
+    table.next[m] = 0
+    table.edge[m] = label
+    table.adjacent[m] = vdx2
     if table.first[vdx1] == 0 then
-      table.first[vdx1] = vdx2
+      table.first[vdx1] = m
     else
-      table.next[] = 
+      table.next[table.last[vdx1]] = m
     end
-    table.last[vdx1] = vdx2
-    return n
+    table.last[vdx1] = m
+    return m
+  end
+  table.resetMark = function()
+    for i,k in pairs(table.mark) do
+      table.mark[i] = false
+    end
   end
   return table
 end
 
 local graph = DataGraph()
 
-function addVertex(label)
-  return graph.addChild(label)
+function addVertex(label,fields)
+  return graph.addVertex(label,fields)
 end
 
 function addEdge(v1,v2,label)
   return graph.addEdge(v1,v2,label)
 end
 
-function printVertex(vertex,fields)
-  print(graph.name(vertex))
-  local adjacent = graph.first(vertex)
-  while adjacent ~= 0 do
-    printVertex(adjacent,fields)
-    adyacent = graph.next(adyacent)
+local function _printVertex(vdx,fields)
+  if graph.mark[vdx] then
+    return
+  end
+  write(graph.vertex[vdx]..' ')
+  if fields then
+    for i=1,#fields do
+      if graph[fields[i]][vdx] then
+        write(graph[fields[i]][vdx]..' ')
+      end
+    end
+  end
+  write('\n')
+  graph.mark[vdx] = true
+  local index = graph.first[vdx]
+  local adjacent = nil
+  while index ~= 0 do
+    adjacent = graph.adjacent[index]
+    _printVertex(adjacent,fields)
+    index = graph.next[index]
   end
 end
 
--- Create a graph programmatically
-
-acme = nodeVertex("Acme Inc.")
-  accounting = addChild(acme,"Accounting")
-    software = addChild(accounting,"New Software")
-    standards = addChild(accounting,"New Accounting Standards")
-  research = addChild(acme,"Research")
-    newProductLine = addChild(research,"New Product Line")
-    newLabs = addChild(research,"New Labs")
-  it = addChild(acme,"IT")
-    outsource = addChild(it,"Outsource")
-    agile = addChild(it,"Go agile")
-    goToR = addChild(it,"Switch to R")
-    
-printNode(acme)
-
--- Custom fields in constructor
-
-birds = nodeVertex("Aves", {vulgo = "Bird"})
-addChild(birds,"Neognathae", {vulgo = "New Jaws", species = 10000})
-addChild(birds,"Palaeognathae", {vulgo = "Old Jaws", species = 60})
-
-printNode(birds, {"vulgo", "species"})
+function printVertex(vdx,fields)
+  graph.resetMark()
+  _printVertex(vdx,fields)
+end
---
+++ test.lua
@@ -0,0 +1,27 @@
+require "dataGraph"
+
+-- Create a graph programmatically
+
+--[[
+
+A->B,A->C,B->D,C->D,D->E,E-F,
+
+--]]
+
+a = addVertex('A',{cost=10,label="First Vertex"})
+b = addVertex('B',{cost=20,label="Second Vertex"})
+c = addVertex('C',{cost=10,label="Third Vertex"})
+d = addVertex('D',{cost=30,label="Fourth Vertex"})
+e = addVertex('E',{cost=10,label="Fifth Vertex"})
+f = addVertex('F',{cost=50,label="Sixth Vertex"})
+
+addEdge(a,b,'10')
+addEdge(a,c,'20')
+addEdge(b,d,'30')
+addEdge(c,d,'40')
+addEdge(d,e,'50')
+addEdge(e,f,'60')
+
+printVertex(a,{'cost','label'})
+print("\n")
+printVertex(b)
