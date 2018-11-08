fb8ccb11a7ee1888996e13a652017feab47e4561
Jason Latouche <96jaslat@gmail.com>
2018-11-07 18:57:22 -0600

    Added ReadFile.lua
    File with necesary functions to read files and parse CSV lines

---
+++ ReadFile.lua
@@ -0,0 +1,64 @@
+-- Functions used to read files
+-- Check if file exits
+-- Get all lines
+-- Parse into list CSV lines
+
+-- see if the file exists
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+end
+
+-- get all lines from a file, returns an empty 
+-- list/table if the file does not exist
+function lines_from(file)
+    if not file_exists(file) then return {} end
+    lines = {}
+    for line in io.lines(file) do 
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+-- get all atributes from a CSV line
+-- it removes "quotes" if the atribute have it
+function ParseCSVLine (line,sep) 
+    local res = {}
+    local pos = 1
+    sep = sep or ','
+    while true do 
+        local c = string.sub(line,pos,pos)
+        if (c == "") then break end
+        if (c == '"') then
+            -- quoted value (ignore separator within)
+            local txt = ""
+            repeat
+                local startp,endp = string.find(line,'^%b""',pos)
+                txt = txt..string.sub(line,startp+1,endp-1)
+                pos = endp + 1
+                c = string.sub(line,pos,pos) 
+                if (c == '"') then txt = txt..'"' end 
+                -- check first char AFTER quoted string, if it is another
+                -- quoted string without separator, then append it
+                -- this is the way to "escape" the quote char in a quote. example:
+                --   value1,"blub""blip""boing",value3  will result in blub"blip"boing  for the middle
+            until (c ~= '"')
+            table.insert(res,txt)
+            assert(c == sep or c == "")
+            pos = pos + 1
+        else	
+            -- no quotes used, just look for the first separator
+            local startp,endp = string.find(line,sep,pos)
+            if (startp) then 
+                table.insert(res,string.sub(line,pos,startp-1))
+                pos = endp + 1
+            else
+                -- no separator found -> use rest of string and terminate
+                table.insert(res,string.sub(line,pos))
+                break
+            end 
+        end
+    end
+    return res
+end
